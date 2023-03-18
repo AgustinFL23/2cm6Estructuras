11aa20b0253bc692e35d19c010d154cb74f8775b
AgustinFL23 <127245897+AgustinFL23@users.noreply.github.com>
2023-03-17 18:44:18 -0600

    Create Pruebas.bat
    Este es el archivo para hacer pruebas, necesitamos cambiar los valores y el nombre de los txt

---
+++ Pruebas.bat
@@ -0,0 +1,14 @@
+gcc misordenamientos.c -c misordenamientos
+gcc main.c misordenamientos.h -o main
+Main.exe 10 <números.txt >>salida.csv
+Main.exe 100 <números.txt >>salida.csv
+Main.exe 500 <números.txt >>salida.csv
+Main.exe 1000 <números.txt >>salida.csv
+Main.exe 5000 <números.txt >>salida.csv
+Main.exe 10000 <números.txt >>salida.csv
+Main.exe 50000 <números.txt >>salida.csv
+Main.exe 100000 <números.txt >>salida.csv
+Main.exe 500000 <números.txt >>salida.csv
+Main.exe 1000000 <números.txt >>salida.csv
+Main.exe 5000000 <números.txt >>salida.csv
+Main.exe 10000000 <números.txt >>salida.csv
