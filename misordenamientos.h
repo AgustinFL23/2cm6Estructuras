fe24433dbd365d66b677f48e682897e04e5d71aa
ArzuGG <108302660+ArzuGG@users.noreply.github.com>
2023-03-26 18:22:02 -0600

    Delete misordenamientos.h

--- misordenamientos.h
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-void Burbuja(int *A, int n);
-void Insercion(int *A,int n);
-void QuickSort(int *A,int p,int r);
-void Seleccion(int *A,int n);
-void MergeSort(int *A,int p,int r);
-
