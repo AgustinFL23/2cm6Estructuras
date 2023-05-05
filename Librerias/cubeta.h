1f47ab73f08ebdd657fb8ccc958f160df8015bb7
AgustinFL23 <127245897+AgustinFL23@users.noreply.github.com>
2023-05-05 13:38:34 -0600

    Delete cubeta.h

--- Librerias/cubeta.h
+++
@@ -1,8 +0,0 @@
-#include "BMP.h" // libreria que nos ayuda a manejar imagenes BMP
-#include <time.h>//libreria que nos permite medir tiempos
-int cubeta (BMP* img,int*RGBO,int x,int y,int* RBGC) ; 
-/*la funcion recibe un apuntador a una estructura BMP que es la imagen que vamos a manipular
-un arreglo de enteros que incluye en los valores Rojo, Azul y verde  del color original en orden
-Las coordenadas x,y en las que  vamos a intentar cambiar el color
-un arreglo de enteros que incluye en los valores Rojo, Azul y verde  del color al que vamos a cambiar en orden*/
-
