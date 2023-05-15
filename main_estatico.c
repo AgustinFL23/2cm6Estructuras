74b210141eaf9c449e8a0a1960f246ddf1d32589
ArzuGG <108302660+ArzuGG@users.noreply.github.com>
2023-05-14 22:12:30 -0600

    Update main_estatico.c

--- main_estatico.c
+++ main_estatico.c
@@ -38,21 +38,23 @@ int main(){
 
 }
 
-float Operar(elemento op1, elemento op2, elemento operador) 
+float Operar(elemento op1, elemento op2, elemento operador) //listo
 {
-    float resultado;
-    switch (operador.dato.caracter) {
+    float resultado;    
+    switch (operador.dato.caracter) // según el operador recibido, se realiza la operación correspondiente
+    {
         case '+':
-            resultado = op1.dato.flotante + op2.dato.flotante;
+            resultado = op1.dato.flotante + op2.dato.flotante; // suma de los operandos
             break;
         case '-':
-            resultado = op1.dato.flotante - op2.dato.flotante;
+            resultado = op1.dato.flotante - op2.dato.flotante; // resta de los operandos
             break;
         case '*':
-            resultado = op1.dato.flotante * op2.dato.flotante;
+            resultado = op1.dato.flotante * op2.dato.flotante; // multiplicación de los operandos
             break;
         case '/':
-            if (op2.dato.flotante == 0) {
+            if (op2.dato.flotante == 0) 
+            {   // se verifica si se intenta dividir por cero
                 printf("Error: División por cero.\n");
                 resultado = 0;
             } else {
@@ -67,7 +69,7 @@ float Operar(elemento op1, elemento op2, elemento operador)
             resultado = 0;
             break;
     }
-    return resultado;
+    return resultado; // se devuelve el resultado de la operación
 }
 
 void ConvertidorPostfijoNumeros(char expresion[]) 
@@ -82,25 +84,40 @@ void ConvertidorPostfijoNumeros(char expresion[])
     for (i = 0; expresion[i] ; i++) {
         c.dato.caracter = expresion[i];
 
-        if (isalpha(c.dato.caracter)) {
+        if (isalpha(c.dato.caracter)) 
+        {
             valor.dato.flotante = AsignarValor(c.dato.caracter);
             push(&valores, valor);
-        } else if (isdigit(c.dato.caracter) || c.dato.caracter == '.') { // Acepta números con decimales
+        } 
+        else if (isdigit(c.dato.caracter) || c.dato.caracter == '.') // Acepta números con decimales
+        { 
+            // Devolver el caracter a la entrada para que pueda ser leído por scanf
             ungetc(c.dato.caracter, stdin);
             scanf("%lf", &valor.dato.flotante); // Utiliza el especificador de formato %lf para números reales
-            push(&valores, valor);
-        } else if (c.dato.caracter == '(') {
+            push(&valores, valor); // Agregar a la pila de valores
+        } 
+        else if (c.dato.caracter == '(') 
+        {
             push(&ope, c);
-        } else if (c.dato.caracter == ')') {
-            while (Top(&ope).dato.caracter != '(') {
-                elemento op = pop(&ope);
-                elemento b = pop(&valores);
-                elemento a = pop(&valores);
-                elemento res = { .tipo = 'f', .dato.flotante = Operar(a, b, op) };
-                push(&valores, res);
+        }
+        // Si es un paréntesis de cierre
+        else if (c.dato.caracter == ')') 
+        {
+            // Mientras que el operador en la cima de la pila no sea un paréntesis de apertura
+            while (Top(&ope).dato.caracter != '(') 
+            {
+                elemento op = pop(&ope);    // Extraer el operador de la pila de operadores
+                elemento b = pop(&valores); // Extraer el segundo operando de la pila de valores
+                elemento a = pop(&valores); // Extraer el primer operando de la pila de valore
+                elemento res = { .tipo = 'f', .dato.flotante = Operar(a, b, op) };  // Realizar la operación correspondiente y crear un nuevo elemento con el resultado
+                push(&valores, res);    // Agregar el resultado a la pila de valores
             }
-            pop(&ope);
-        } else {
+
+            pop(&ope);  // Eliminar el paréntesis de apertura de la pila de operadores
+        }
+        else 
+        {   
+        // Mientras que haya operadores en la pila de operadores y la prioridad del operador en la cima de la pila sea mayor o igual que la del operador actual
             while (ope.tope > 0 && Prioridad(Top(&ope).dato.caracter) >= Prioridad(c.dato.caracter)) {
                 elemento op = pop(&ope);
                 elemento b = pop(&valores);
@@ -111,10 +128,12 @@ void ConvertidorPostfijoNumeros(char expresion[])
             push(&ope, c);
         }
     }
-    while (ope.tope > 0) {
+    while (ope.tope > 0) //mientras nuestra pila no este vacia
+    {
         elemento op = pop(&ope);
         elemento b = pop(&valores);
         elemento a = pop(&valores);
+        // Realizar la operación correspondiente y crear un nuevo elemento con el resultado
         elemento res = { .tipo = 'f', .dato.flotante = Operar(a, b, op) };
         push(&valores, res);
     }
@@ -123,8 +142,8 @@ void ConvertidorPostfijoNumeros(char expresion[])
 
 float AsignarValor(char letra) 
 {
-    int indice = letra - 'A';
-    if (valores[indice] != 0) 
+    int indice = letra - 'A';   //obtenemos el indice de lla variable en el arreglo valores
+    if (valores[indice] != 0)   //si la variable ya tiene asignado un valor
     {
         printf("La letra %c ya tiene asignado un valor \n", letra);
         return valores[indice];
@@ -133,8 +152,8 @@ float AsignarValor(char letra)
     {
         printf("Ingrese el valor de la letra %c:\n", letra);
         float valor;
-        scanf("%f", &valor);
-        valores[indice] = valor;
+        scanf("%f", &valor);    //guardamos el valor tipo flotante en la letra
+        valores[indice] = valor;    //lo guardamos en el arreglo
         return valor;
     }
 }
@@ -143,14 +162,14 @@ int Prioridad(char c) //listo
 {
     switch(c) {
         case '^':
-            return 3;
+            return 3;   //Maypr prioridad
         case '*':
             return 2;
         case '/':
             return 2;
         case '+':
         case '-':
-            return 1;
+            return 1;   //Menor prioridad
         default:
             return -1;
     }
@@ -165,14 +184,15 @@ int ComprobarParentesis(char expresion[])
 
     for (i = 0; expresion[i] != '\0'; i++) {
         c.tipo = 'c'; // indicamos que el tipo de dato de la estructura es un caracter
-        c.dato.caracter = expresion[i];
-        if (c.dato.caracter == '(') 
+        c.dato.caracter = expresion[i]; //guardamos el caracter en la estructura
+        if (c.dato.caracter == '(') //si es un parentesis de apertura se guarda en la pila
         {
             push(&p, c);
         } 
-        else if (c.dato.caracter == ')') 
+        else if (c.dato.caracter == ')') //si es un parentesis de cerradura
         {
-            if (p.tope == 0 || pop(&p).dato.caracter != '(') {
+            if (p.tope == 0 || pop(&p).dato.caracter != '(') //si no encontramos el parentesis correspondiente al de apertura o nuestro pila esta vacia retornamos 0 y nuestra expresion no esta balanceada
+            {
                 return 0;
             }
         }
@@ -189,13 +209,17 @@ void ConvertidorPostfijo(char expresion[])
 
     for (i = 0; expresion[i] != '\0'; i++) {
         c.dato.caracter = expresion[i];
-        if (isalpha(c.dato.caracter)) {
+        if (isalpha(c.dato.caracter)) // Si el caracter es una letra, simplemente lo imprime
+        {
             printf("%c", c.dato.caracter);
         } 
+        // Si el caracter es un paréntesis abierto, lo agrega a la pila de operadores
         else if (c.dato.caracter == '(') 
         {
             push(&ope, c);
         } 
+        // Si el caracter es un paréntesis cerrado, desapila operadores hasta encontrar el paréntesis abierto
+        // e imprime los operadores desapilados
         else if (c.dato.caracter == ')') 
         {
             while (Top(&ope).dato.caracter != '(') 
@@ -204,6 +228,8 @@ void ConvertidorPostfijo(char expresion[])
             }
             pop(&ope);
         } 
+        // Si el caracter es un operador, desapila operadores con igual o mayor prioridad
+        // y los imprime. Luego agrega el operador actual a la pila de operadores.
         else {
             while (ope.tope > 0 && Prioridad(Top(&ope).dato.caracter) >= Prioridad(c.dato.caracter)) {
                 printf("%c", pop(&ope).dato.caracter);
@@ -211,7 +237,9 @@ void ConvertidorPostfijo(char expresion[])
             push(&ope, c);
         }
     }
-    while (ope.tope > 0) {
+    // Desapila y imprime todos los operadores restantes en la pila de operadores
+    while (ope.tope > 0) 
+    {
         printf("%c", pop(&ope).dato.caracter);
     }
     printf("\n");
