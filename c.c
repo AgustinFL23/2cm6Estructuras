f21c3f376400ac1b9482f99b4b70a0f5aadaa013
ArzuGG <108302660+ArzuGG@users.noreply.github.com>
2023-06-15 19:18:55 -0600

    Add files via upload

---
+++ c.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "TADListaSL.h"
+
+void cargar(lista *diccionario);
+void agregar(lista *diccionario);
+void buscar(lista *diccionario);
+void modificar(lista *diccionario);
+void eliminar(lista *diccionario);
+void estadisticas(lista *diccionario);
+
+int main(){
+    
+    
+
+
+}
+
+void cargar(lista *diccionario);
+void agregar(lista *diccionario);
+void buscar(lista *diccionario);
+void modificar(lista *diccionario);
+void eliminar(lista *diccionario);
+void estadisticas(lista *diccionario);
